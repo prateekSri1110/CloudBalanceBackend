d1fb92b3805b5c8e7fc9cafe405cb6c802334d40
Prateek Srivastava <prateek.kumar.srivastava.1110@gmail.com>
2026-01-12 14:55:43 +0530

    new changes

---
+++ src/main/java/com/CloudKeeper/CloudBalanceBackend/modal/CEdataDTO.java
@@ -0,0 +1,13 @@
+package com.CloudKeeper.CloudBalanceBackend.modal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
+public class CEdataDTO {
+    private String type;
+//    Map<String,Object>
+}
---
+++ src/main/java/com/CloudKeeper/CloudBalanceBackend/modal/ProfileDTO.java
@@ -0,0 +1,12 @@
+package com.CloudKeeper.CloudBalanceBackend.modal;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ProfileDTO {
+    String name;
+    String role;
+}
